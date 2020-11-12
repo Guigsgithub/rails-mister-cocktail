3002a90dc29a3d5c9ad9a41117a6ca3a1b95b4bc
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-12 11:58:41 +0100

    models cocktail ingredient dose created

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-const { environment } = require('@rails/webpacker')
-
 # Initialize the Rails application.
 Rails.application.initialize!
