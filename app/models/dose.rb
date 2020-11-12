d17de1bc68f542f4f272e8e7d3bd9d04083a0e09
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-12 17:52:05 +0100

    uniqueness updated

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, presence: true
+  validates :description, presence: { message: "Vous devez saisir une description" }
 
-  validates :ingredient_id, uniqueness: { scope: :cocktail_id }
+  validates :ingredient_id, uniqueness: { scope: :cocktail_id, message: "Cet ingrédient a déjà été ajouté" }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
 
-  validates :name, uniqueness: true
-  validates :name, presence: true
+  validates :name, uniqueness: { message: "Cet ingrédient existe déjà" }
+  validates :name, presence: { message: "Vous devez saisir un ingrédient" }
 end
