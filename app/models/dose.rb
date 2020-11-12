ebd824a3b74fd2d10b47e814c1c9aaabc9d7e3d2
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-12 12:32:17 +0100

    controllers cocktails and doses generated

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,8 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
+  # validates :cocktail_id, presence: true
+  # validates :ingredient_id, presence: true
+
   validates :cocktail_id && :ingredient_id, uniqueness: true
 end
